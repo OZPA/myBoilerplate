1a1d1a1fb6171671c50bb5d930badd2f2f490874
Kenichi Hasegawa <hase0023@gmail.com>
2015-12-29 10:09:29 +0900

    head内、meta name="keywords"を削除

--- inquiry/inquiry-send.php
+++ inquiry/inquiry-send.php
@@ -3,7 +3,6 @@
 <head>
   <meta charset="UTF-8">
   <meta name="description" content="">
-  <meta name="keywords" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
   <link rel="canonical" href="" />
   <title>お問い合わせ頂き誠にありがとうございました｜サイト名</title>
